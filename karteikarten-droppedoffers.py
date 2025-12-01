7d9f4fa11269bebfb8d383d53aa5725e209e5b21
Patty <119142482+pattyintheshell@users.noreply.github.com>
2025-12-01 19:26:09 +0100

    Refine questions and answers in karteikarten

--- karteikarten-droppedoffers.py
+++ karteikarten-droppedoffers.py
@@ -6,16 +6,25 @@ karten = [
     {"frage": "Quali: Der Kandidat sagt, er will wechseln, weil es seinem aktuellen Arbeitgeber wirtschaftlich schlecht geht. Was gibt es zu beachten?",
      "antwort": "Nach einem möglichen Abfindungsprogramm / Freiwilligenprogramm fragen"},
     {"frage": "Quali: Der Kandidat sagt, er will wechseln, weil ihm die Herausforderung fehlt und er sich weiterentwickeln will (z.B. zum Senior Entwickler, Softwarearchitekt o.ä.)",
-     "antwort": "Hat er sich intern beworben? Gibt es intern andere Teams/Abteilungen/Produktsparten, die ihn reizen würden? Was könnte sein Arbeitgeber ihm noch anbieten?"}, 
+     "antwort": "Hat er sich intern beworben? Gibt es intern andere Teams/Abteilungen/Produktsparten, die ihn reizen würden? Was könnte sein Arbeitgeber ihm noch anbieten (nicht finanziell)?"}, 
     {"frage": "Wie lässt sich eine mögliche Abfindung grob berechnen?",
      "antwort": "Betriebsjahre * Monatsgehalt (Faktor 0,5–1,5)"},
-    {"frage": "Nach dem 1st oder Final Interview: Der Kandidat hat andere Prozesse (nicht über uns) und lässt sich nicht festnageln. Was tun?",
+    {"frage": "Kandidat hat andere Prozesse (nicht über uns) und lässt sich nach dem Interview nicht festnageln. Was tun?",
      "antwort": "Kandidat: Fear of Loss, andere Kandidaten on hold // Kunde: transparent sein, dass der Kandidat wenig committed ist, idealerweise andere Profile einsellen // Präventiv: in der Quali Exclusivity holen"},
-    {"frage": "Der Kandidat hat dir in der Quali gesagt, sein Traumjob ist in der Medizintechnik. Er hatte nun ein Final bei deinem Kunden aus der Industriellen Automatisierung. Zudem hat er noch andere Prozesse (nicht über uns) in der Medizintechnik, die zeitlich nicht so weit sind. Der Kandidat sagt nach dem Final mit deinem Kunden, er fand das Gespräch gut, scheint aber nicht 100% richtig begeistert. Wie machst du hier weiter?",
-     "antwort": "Den Kandidaten ehrlich fragen: Wenn er seine anderen Prozesse nicht hätte, würde er es dann machen wollen? // Herausfinden, ob es nur ein Plan B / Notfall ist"},
-    {"frage": "Der Kunde will das Offer unbedingt selbst verschicken bzw. es ist aus Datenschutzgründen o.ä. nicht möglich, das Offer über uns zu senden. Was tun?",
-     "antwort": "Den Kunden überzeugen, das Offer zumindest erst nach Absprache mit uns zu versenden. // Zudem fragen: Gibt es etwas im Vertrag, was Erklärungsbedarf braucht? (Überstundenregelung, Urlaubstage, Jobtitel, Homeoffice)"}
+    {"frage": "Der Kandidat hat in der Quali gesagt, sein Traumjob ist in der Medizintechnik. Er hatte nun ein Final bei deinem Kunden aus einer anderen Branche. Zudem hat er noch andere Prozesse (nicht über uns) in der Medizintechnik, die noch ganz am Anfang sind. Der Kandidat sagt nach dem Final mit deinem Kunden, er fand das Gespräch gut, scheint aber nicht 100% begeistert. Was tun?",
+     "antwort": "Den Kandidaten ehrlich fragen: Wenn er seine anderen Prozesse nicht hätte, würde er es dann machen wollen? // Herausfinden, ob es nur ein Plan B oder Backup ist"},
+    {"frage": "Der Kunde will das Angebot unbedingt selbst an den Kandidaten schicken oder es ist aus Datenschutzgründen o.ä. nicht möglich, dies über uns zu machen. Was tun?",
+     "antwort": "Den Kunden überzeugen, das Offer zumindest erst nach Absprache mit uns zu versenden. // Zudem erfragen: Gibt es etwas im Vertrag, was Erklärungsbedarf braucht? (Überstundenregelung, Urlaubstage, Jobtitel, Homeoffice)"}
+    {"frage": "Welche Infos solltest du bei der Quali unbedingt abfragen?",
+     "antwort": "Push & Pull Factors, andere Prozesse, Standort/Umzug/Pendeln/Homeoffice/Familie, Gegenangebote, Exclusivity"},
+    {"frage": "Wie gehst du mit Kandidaten um, die mehrere Prozesse parallel laufen haben?",
+     "antwort": "Fear of Loss, transparente Kommunikation an den Kunden, andere Kandidaten einsellen, durch Exclusivity vorbeugen"},
+    {"frage": "Was sind Argumente dagegen, auf ein Gegenangebot einzugehen?",
+     "antwort": "Die eigentlichen Gründe der Unzufriedenheit ändern sich nicht (Push Factors aus der Quali) // Die meisten, die darauf eingehen, sind nach ein paar Monaten wieder offen // Versprechungen in der Hitze des Gefechts, die nicht gehalten werden können // Loyalität wird infrage gestellt, der erste auf der „Abschussliste“ bei finanziellen Probleme // ist nur eine Panikhandlung vom Arbeitgeber, da Nachbesetzung und unbesetzte Stellen sehr teuer"},
+    {"frage": "Wann sollte das Thema Counter Offer angesprochen werden?",
+     "antwort": "Bereits in der Quali"}
 ]
+
 
 # --- Session State für zufällige Reihenfolge ---
 if "order" not in st.session_state:
